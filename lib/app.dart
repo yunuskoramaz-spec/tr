8c26dd3d2d0fee972614ff09fc7e4b80bb33878d
yunuskoramaz-spec <yunuskoramaz@gmail.com>
2026-09-08 02:25:37 +0300

    Kayseri Kurye yönlendirme ve tema altyapısını ekle

---
+++ lib/app.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'features/home/home_page.dart';
+import 'features/search/address_search_page.dart';
+import 'features/duty/duty_page.dart';
+
+class KayseriKuryeApp extends StatelessWidget {
+  const KayseriKuryeApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final router = GoRouter(
+      routes: [
+        GoRoute(path: '/', builder: (_, __) => const HomePage()),
+        GoRoute(path: '/adres', builder: (_, __) => const AddressSearchPage()),
+        GoRoute(path: '/nobet', builder: (_, __) => const DutyPage()),
+      ],
+    );
+    return MaterialApp.router(
+      title: 'Kayseri Kurye',
+      theme: ThemeData(
+        useMaterial3: true,
+        colorSchemeSeed: Colors.deepOrange,
+        scaffoldBackgroundColor: const Color(0xFFF7F7F7),
+      ),
+      routerConfig: router,
+    );
+  }
+}
