5b428d7714f7ae0cccc0c55aed0aa57f271387ab
yunuskoramaz-spec <yunuskoramaz@gmail.com>
2026-09-08 02:02:59 +0300

    Add main activity

---
+++ app/src/main/java/com/yunus/tr81/MainActivity.java
@@ -0,0 +1,22 @@
+package com.yunus.tr81;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends Activity {
+    @Override public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        WebView webView = new WebView(this);
+        WebSettings settings = webView.getSettings();
+        settings.setJavaScriptEnabled(true);
+        settings.setDomStorageEnabled(true);
+        settings.setBuiltInZoomControls(false);
+        settings.setDisplayZoomControls(false);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("file:///android_asset/index.html");
+        setContentView(webView);
+    }
+}
