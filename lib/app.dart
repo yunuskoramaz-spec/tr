3ae46e5a0350126e6f8c39c5f4c95e3133387e1d
yunuskoramaz-spec <yunuskoramaz@gmail.com>
2026-09-08 02:33:35 +0300

    Refactor app navigation for Kayseri directory

--- lib/app.dart
+++ lib/app.dart
@@ -3,6 +3,7 @@ import 'package:go_router/go_router.dart';
 import 'features/home/home_page.dart';
 import 'features/search/address_search_page.dart';
 import 'features/duty/duty_page.dart';
+import 'features/places/place_category_page.dart';
 
 class KayseriKuryeApp extends StatelessWidget {
   const KayseriKuryeApp({super.key});
@@ -14,10 +15,15 @@ class KayseriKuryeApp extends StatelessWidget {
         GoRoute(path: '/', builder: (_, __) => const HomePage()),
         GoRoute(path: '/adres', builder: (_, __) => const AddressSearchPage()),
         GoRoute(path: '/nobet', builder: (_, __) => const DutyPage()),
+        GoRoute(
+          path: '/kategori/:type',
+          builder: (_, state) => PlaceCategoryPage(type: state.pathParameters['type']!),
+        ),
       ],
     );
     return MaterialApp.router(
       title: 'Kayseri Kurye',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         useMaterial3: true,
         colorSchemeSeed: Colors.deepOrange,
